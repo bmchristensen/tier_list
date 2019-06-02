3ae9d37c77304c9aac5d043c34f9b0a2419e6b10
bmchristensen <bmchristensen@gmail.com>
2019-06-01 19:11:48 -0700

    Workflow Tests: Update creates_list_spec to attach 5 champions to satisfy list length enforcement.
    Enforcing champions list length with custom ActiveRecord validator was not allowing the creates_list_spec 'attaches champions to the list' test to successfully build the list and not be a new record. With a test list of 5 champions parsed into the new list we can expect the list size to be 5 and the record to not be new since it adds them to the 'creator' instantiated on line 4. References #6.

--- app/workflows/creates_list.rb
+++ app/workflows/creates_list.rb
@@ -1,7 +1,7 @@
 class CreatesList
   attr_accessor :name, :list, :champ_string
 
-  def initialize(name: "", champ_string: "")
+  def initialize(name: '', champ_string: '')
     @name = name
     @champ_string = champ_string
     @success = false
@@ -25,7 +25,7 @@ class CreatesList
 
   def convert_string_to_champion
     champ_string.split("\n").map do |one_champ|
-      champ_name, tier = one_champ.split(":")
+      champ_name, tier = one_champ.split(':')
       Champion.new(name: champ_name, tier: tier)
     end
   end
