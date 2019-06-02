f100a0e172e0ceb382398952c8ef9333b65a9d66
bmchristensen <bmchristensen@gmail.com>
2019-06-01 19:27:24 -0700

    System Test: Update system tests to meet list requirement of exactly 5 champions.
    System tests were adding only 2 champions for each test. Application Record enforces a requirement of exactly 5 champions per list. The system tests were updated to have 5 test champions each. Expected sizes adjusted accordingly. References #8.

--- spec/system/add_champion_spec.rb
+++ spec/system/add_champion_spec.rb
@@ -1,17 +1,17 @@
 require "rails_helper"
 
 RSpec.describe "adding a champion", type: :system do
-  it "allows a user to create a list of champions" do
+  it "allows a user to create a valid list of champions" do
     visit new_list_path
     fill_in "Name", with: "Mid Lane"
-    fill_in "Champions", with: "Zed:S\nKatarina:C"
+    fill_in "Champions", with: "Zed:S\nKatarina:C\nJinx:A\nLee_Sin:B\nAhri:S"
     click_on("Create List")
     visit lists_path
     @list = List.find_by(name: "Mid Lane")
     expect(page).to have_selector(
       "#list_#{@list.id} .name", text: "Mid Lane")
     expect(page).to have_selector(
-      "#list_#{@list.id} .size", text: "2")
+      "#list_#{@list.id} .size", text: "5")
   end
 
   it "does not allow a user to create a list without a name" do
@@ -26,30 +26,30 @@ RSpec.describe "adding a champion", type: :system do
   it "allows a user to create multiple lists of champions, and navigate to each one" do
     visit new_list_path
     fill_in "Name", with: "Mid Lane"
-    fill_in "Champions", with: "Zed:S\nKatarina:C"
+    fill_in "Champions", with: "Zed:S\nKatarina:C\nJinx:A\nLee_Sin:B\nAhri:S"
     click_on("Create List")
     visit lists_path
     @list_a = List.find_by(name: "Mid Lane")
     expect(page).to have_selector(
       "#list_#{@list_a.id} .name", text: "Mid Lane")
     expect(page).to have_selector(
-      "#list_#{@list_a.id} .size", text: "2")
+      "#list_#{@list_a.id} .size", text: "5")
     visit new_list_path
     fill_in "Name", with: "Top Lane"
-    fill_in "Champions", with: "Riven:A\nTeemo:B"
+    fill_in "Champions", with: "Riven:A\nTeemo:B\nJinx:A\nLee_Sin:B\nAhre:S"
     click_on("Create List")
     visit lists_path
     @list_b = List.find_by(name: "Top Lane")
     expect(page).to have_selector(
       "#list_#{@list_b.id} .name", text: "Top Lane")
     expect(page).to have_selector(
-      "#list_#{@list_b.id} .size", text: "2")
+      "#list_#{@list_b.id} .size", text: "5")
     visit lists_path
     @list_a = List.find_by(name: "Mid Lane")
     expect(page).to have_selector(
       "#list_#{@list_a.id} .name", text: "Mid Lane")
     expect(page).to have_selector(
-      "#list_#{@list_a.id} .size", text: "2")
+      "#list_#{@list_a.id} .size", text: "5")
 
   end
 
