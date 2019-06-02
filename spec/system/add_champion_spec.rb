ef73bb464de4f8d71f03c8c4389aaacfa29cc281
bmchristensen <bmchristensen@gmail.com>
2019-06-01 19:37:45 -0700

    System Test: Improve formatting and readability.
    Use Ruby community guidelines for standards. Update formatting and naming for readability. References #8.

--- spec/system/add_champion_spec.rb
+++ spec/system/add_champion_spec.rb
@@ -1,65 +1,73 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "adding a champion", type: :system do
-  it "allows a user to create a valid list of champions" do
+RSpec.describe 'adding a champion', type: :system do
+  it 'allows a user to create a valid list of champions' do
     visit new_list_path
-    fill_in "Name", with: "Mid Lane"
-    fill_in "Champions", with: "Zed:S\nKatarina:C\nJinx:A\nLee_Sin:B\nAhri:S"
-    click_on("Create List")
+    fill_in 'Name', with: 'Mid Lane'
+    fill_in 'Champions', with: "Zed:S\nKatarina:C\nJinx:A\nLee_Sin:B\nAhri:S"
+    click_on('Create List')
     visit lists_path
-    @list = List.find_by(name: "Mid Lane")
+    @list = List.find_by(name: 'Mid Lane')
     expect(page).to have_selector(
-      "#list_#{@list.id} .name", text: "Mid Lane")
+      "#list_#{@list.id} .name", text: 'Mid Lane')
     expect(page).to have_selector(
-      "#list_#{@list.id} .size", text: "5")
+      "#list_#{@list.id} .size", text: '5')
   end
 
-  it "does not allow a user to create a list without a name" do
+  it 'does not allow a user to create a list without a name' do
     visit new_list_path
-    fill_in "Name", with: ""
-    fill_in "Champions", with: "Zed:S\nKatarina:C"
-    click_on("Create List")
-    expect(page).to have_selector(".new_list")
+    fill_in 'Name', with: ''
+    fill_in 'Champions', with: "Zed:S\nKatarina:C"
+    click_on('Create List')
+    expect(page).to have_selector('.new_list')
     expect(page).to have_text("Name can't be blank")
   end
 
-  it "allows a user to create multiple lists of champions, and navigate to each one" do
+  it 'allows a user to create multiple champion lists and navigate to them' do
     visit new_list_path
-    fill_in "Name", with: "Mid Lane"
-    fill_in "Champions", with: "Zed:S\nKatarina:C\nJinx:A\nLee_Sin:B\nAhri:S"
-    click_on("Create List")
-    visit lists_path
-    @list_a = List.find_by(name: "Mid Lane")
-    expect(page).to have_selector(
-      "#list_#{@list_a.id} .name", text: "Mid Lane")
-    expect(page).to have_selector(
-      "#list_#{@list_a.id} .size", text: "5")
-    visit new_list_path
-    fill_in "Name", with: "Top Lane"
-    fill_in "Champions", with: "Riven:A\nTeemo:B\nJinx:A\nLee_Sin:B\nAhre:S"
-    click_on("Create List")
-    visit lists_path
-    @list_b = List.find_by(name: "Top Lane")
-    expect(page).to have_selector(
-      "#list_#{@list_b.id} .name", text: "Top Lane")
-    expect(page).to have_selector(
-      "#list_#{@list_b.id} .size", text: "5")
-    visit lists_path
-    @list_a = List.find_by(name: "Mid Lane")
-    expect(page).to have_selector(
-      "#list_#{@list_a.id} .name", text: "Mid Lane")
-    expect(page).to have_selector(
-      "#list_#{@list_a.id} .size", text: "5")
+    fill_in 'Name', with: 'Mid Lane'
+    fill_in 'Champions', with: "Zed:S\nKatarina:C\nJinx:A\nLee_Sin:B\nAhri:S"
+    click_on('Create List')
 
+    visit lists_path
+    @list_a = List.find_by(name: 'Mid Lane')
+    expect(page).to have_selector(
+      "#list_#{@list_a.id} .name", text: 'Mid Lane'
+    )
+    expect(page).to have_selector(
+      "#list_#{@list_a.id} .size", text: '5'
+    )
+
+    visit new_list_path
+    fill_in 'Name', with: 'Top Lane'
+    fill_in 'Champions', with: "Riven:A\nTeemo:B\nJinx:A\nLee_Sin:B\nAhre:S"
+    click_on('Create List')
+
+    visit lists_path
+    @list_b = List.find_by(name: 'Top Lane')
+    expect(page).to have_selector(
+      "#list_#{@list_b.id} .name", text: 'Top Lane'
+    )
+    expect(page).to have_selector(
+      "#list_#{@list_b.id} .size", text: '5'
+    )
+
+    visit lists_path
+    @list_a = List.find_by(name: 'Mid Lane')
+    expect(page).to have_selector(
+      "#list_#{@list_a.id} .name", text: 'Mid Lane'
+    )
+    expect(page).to have_selector(
+      "#list_#{@list_a.id} .size", text: '5'
+    )
   end
 
-  it "does not allow a user to create a list with a single champion string, and reverts user to original new list page" do
+  it 'does not allow user to create an invalid list, revert to new list page' do
     visit new_list_path
-    fill_in "Name", with: "Mid Lane"
-    fill_in "Champions", with: "Zed"
-    click_on("Create List")
-    expect(page).to have_selector(".new_list")
-    expect(page).to have_text("Champions is invalid")
+    fill_in 'Name', with: 'Mid Lane'
+    fill_in 'Champions', with: 'Zed'
+    click_on('Create List')
+    expect(page).to have_selector('.new_list')
+    expect(page).to have_text('Champions is invalid')
   end
-
 end
