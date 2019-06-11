ea76ef79f17c63e2fb29927f8234c067b7fbeedf
bmchristensen <bmchristensen@gmail.com>
2019-06-10 20:52:27 -0700

    Code Convention: Use single quotes when nothing else is nested. References #12.

--- spec/factories/champion.rb
+++ spec/factories/champion.rb
@@ -1,76 +1,76 @@
 FactoryBot.define do
   factory :champion do
-    name { "Name" }
-    tier { "S" }
-    role { "ADC" }
+    name { 'Name' }
+    tier { 'S' }
+    role { 'ADC' }
     size { 1 }
 
     trait :invalid_tier do
-      tier { "BadTier" }
+      tier { 'BadTier' }
     end
 
     trait :invalid_name do
-      name {""}
+      name { '' }
     end
 
     trait :katarina do
-      name { "Katarina" }
-      tier { "C" }
-      role { "Mid" }
+      name { 'Katarina' }
+      tier { 'C' }
+      role { 'Mid' }
     end
 
     trait :zed do
-      name { "Zed" }
-      tier { "S" }
-      role { "Mid" }
+      name { 'Zed' }
+      tier { 'S' }
+      role { 'Mid' }
     end
 
     trait :lucian do
-      name { "Lucian" }
-      tier { "B" }
-      role { "ADC" }
+      name { 'Lucian' }
+      tier { 'B' }
+      role { 'ADC' }
     end
 
     trait :jax do
-      name { "Jax" }
-      tier { "A" }
-      role { "Top" }
+      name { 'Jax' }
+      tier { 'A' }
+      role { 'Top' }
     end
 
     trait :draven do
-      name { "Draven" }
-      tier { "A" }
-      role { "ADC" }
+      name { 'Draven' }
+      tier { 'A' }
+      role { 'ADC' }
     end
 
     trait :ahri do
-      name { "Ahri" }
-      tier { "S" }
-      role { "Mid" }
+      name { 'Ahri' }
+      tier { 'S' }
+      role { 'Mid' }
     end
 
     trait :hecarim do
-      name { "Hecarim" }
-      tier { "S" }
-      role { "Jungle" }
+      name { 'Hecarim' }
+      tier { 'S' }
+      role { 'Jungle' }
     end
 
     trait :jinx do
-      name { "Jinx" }
-      tier { "S" }
-      role { "ADC" }
+      name { 'Jinx' }
+      tier { 'S' }
+      role { 'ADC' }
     end
 
     trait :lee_sin do
-      name { "Lee Sin" }
-      tier { "S" }
-      role { "Jungle" }
+      name { 'Lee Sin' }
+      tier { 'S' }
+      role { 'Jungle' }
     end
 
     trait :soraka do
-      name { "Soraka" }
-      tier { "A" }
-      role { "Support" }
+      name { 'Soraka' }
+      tier { 'A' }
+      role { 'Support' }
     end
   end
 end
